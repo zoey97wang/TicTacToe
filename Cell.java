fe02f6177c1aa302e6eb9ed6f42713d8f09615c1
zoeywang <zwang297@gmail.com>
2017-02-26 23:44:19 -0500

    OO version -------  Board's Cell

--- Cell.java
+++ Cell.java
@@ -5,7 +5,7 @@ public class Cell {
 	Seed content;
 	int col, row;   //row and col of this seed not used for now;
 	
-	public Cell(int row, int col){
+	public Cell(int row, int col){  //constructor
 		this.row = row;
 		this.col = col;
 		clear();
@@ -17,9 +17,9 @@ public class Cell {
 	
 	public void paint(){
 		switch(content){
-			case EMPTY: System.out.println(" "); break;
-			case CROSS: System.out.println("X"); break;
-			case CIRCLE: System.out.println("O"); break;
+			case EMPTY: System.out.print(" "); break;
+			case CROSS: System.out.print("X"); break;
+			case CIRCLE: System.out.print("O"); break;
 		}
 	}
 }
